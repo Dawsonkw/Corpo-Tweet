684982c5da7002ea4d2deaa4d5c04415f805bb07
Dawsonkw <dawsonkw@gmail.com>
2024-05-13 17:12:49 -0600

    added in an error message component, a spinner component, and added in a useEffect hook to fetch the feedback data from the server.

--- src/components/FeedbackList.tsx
+++ src/components/FeedbackList.tsx
@@ -1,34 +1,45 @@
+import { useEffect, useState } from "react";
 import FeedbackItem from "./FeedbackItem";
-
-const feedbackItems = [
-  {
-    upvoteCount: 420,
-    badgeLetter: "T",
-    companyName: "Talor",
-    text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus expedita facilis odit in, iste architecto.",
-    daysAgo: 69,
-  },
-  {
-    upvoteCount: 69,
-    badgeLetter: "G",
-    companyName: "Garl",
-    text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus expedita facilis odit in, iste architecto.",
-    daysAgo: 23,
-  },
-  {
-    upvoteCount: 593,
-    badgeLetter: "D",
-    companyName: "Dawson",
-    text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus expedita facilis odit in, iste architecto.",
-    daysAgo: 4,
-  },
-];
+import Spinner from "./Spinner";
+import ErrorMessage from "./ErrorMessage";
 
 function FeedbackList() {
+  const [feedbackItems, setFeedbackItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  useEffect(() => {
+    const fetchFeedbackItems = async () => {
+        setIsLoading(true);
+        try {
+            const response = await fetch(
+              "https://bytegrad.com/course-assets/projects/corpcomment/api/feedbacks"
+            );
+      
+            if (!response.ok) {
+              throw new Error();
+            }
+            
+            const data = await response.json();
+            setFeedbackItems(data.feedbacks);
+        } catch (error) {
+            setErrorMessage("Failed to load feedback items");
+        }
+        setIsLoading(false);
+    };
+    
+    fetchFeedbackItems();
+  }, []);
+
   return (
     <ol className="feedback-list">
+      {isLoading && <Spinner />}
+      {errorMessage && <ErrorMessage message={errorMessage} />}
+
       {feedbackItems.map((feedbackItem) => {
-        return <FeedbackItem feedbackItem={feedbackItem} />;
+        return (
+          <FeedbackItem key={feedbackItem.id} feedbackItem={feedbackItem} />
+        );
       })}
     </ol>
   );
