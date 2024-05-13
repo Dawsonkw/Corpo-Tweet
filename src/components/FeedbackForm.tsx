bd0940fc53b733b524028e37a7e8491f1e117055
Dawsonkw <dawsonkw@gmail.com>
2024-05-13 07:54:00 -0600

    updated counter to use a constant value, changed the text area to a controlled text component

--- src/components/FeedbackForm.tsx
+++ src/components/FeedbackForm.tsx
@@ -1,16 +1,31 @@
+import { useState } from "react";
+import { MAX_CHARACTERS } from "../lib/constants";
+
+
+
 function FeedbackForm() {
+  const [text, setText] = useState("");
+
+  const charCount = MAX_CHARACTERS - text.length;
+
   return (
     <form className="form">
+      {/* text area is a controlled input using the useState hook.*/}
       <textarea
+        value={text}
+        onChange={(event) => {
+          setText(event.target.value);
+        }}
         id="feedback-textarea"
         placeholder="feedbackPlaceholder"
         spellCheck={false}
       />
+
       <label htmlFor="feedback-textarea">
         enter your feedback here, remember to #hashtag the company!
       </label>
       <div>
-        <p className="u-italic">150</p>
+        <p className="u-italic">{charCount}</p>
         <button>
           <span>Submit</span>
         </button>
